83da87ee0455aedb3b8c7a8907b804baa76dfa5a
minkeejoo <mksu234@naver.com>
2020-12-14 19:16:24 +0900

    commit

---
+++ exampleweb/polls/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+
+    # Example : polls/
+    path('', views.index, name="index"),
+
+    # Example : polls/1 or polls/2 or polls/3 or .... polls/{n}
+    path('<int:question_id>/', views.detail, name ='detail'), # <자료형:변수이름>
+
+    # Example : polls/1/vote or polls/2/vote or polls/3/vote ...
+    path('<int:question_id>/vote/', views.vote, name ='vote'),
+    
+    # Example : polls/1/results or polls/2/results or polls/3/results ...
+    path('<int:question_id>/results/', views.results, name ='results')
+]
---
+++ exampleweb/polls/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from polls.repository import PollsRepository
+# Create your views here.
+
+def index(request):
+
+    repository = PollsRepository()
+    question_list = repository.select_questions()
+
+    # print(question_list)
+    context = { 'question_list' : question_list}
+
+    return render(request, "polls/index.html", context)  # context는 index.html을 처리할 때 사용할 수 있도록 전달하는 데이터
+
+def detail(request, question_id):
+
+    # 테스트용 코드
+    # return HttpResponse("<h1>Question Detail {0} Page</h1>".format(question_id))
+
+    # 1. Question 조회 + Choice 조회
+    repository = PollsRepository()
+    question = repository.select_question_by_id(question_id)
+    choice_list = repository.select_choice_by_question_id(question_id)
+    question.choices = choice_list
+
+    # 2. 템플릿으로 이동 (데이터도 함께 전달)
+    return render(request, "polls/detail.html", { 'question' : question })
+
+def vote(request, question_id):
+    # 테스트용 코드
+    # return HttpResponse("<h1>Vote for Question {0} Page</h1>".format(question_id))
+
+    # 사용자가 form에 입력해서 POST로 전송한 choice 값 읽기
+    choice_id = int(request.POST['choice']) # request.POST['name 속성의 값'] : POST로 전송된 요청 데이터를 읽는 방법 -> 문자열
+    
+    # 사용자가 선택한 choice의 votes값을 1 증가 (데이터베이스 변경 사항)
+    repository = PollsRepository()
+    repository.update_choice_votes(choice_id)
+
+    # 투표 결과 화면으로 이동 (redirect 이동 : 브라우저로 응답을 보내는데 이 응답이 즉시 다시 서버로 새로운 요청을 보내도록 처리)
+    return HttpResponseRedirect(reverse("polls:results", args=(question_id, ))) # reverse : urls.py에 등록된 요청경로를 가져와서 사용
+
+def results(request, question_id):
+    # 테스트용 코드
+    return HttpResponse("<h1>Result for Question {0} Page</h1>".format(question_id))
+
+   
+
+    
+    
