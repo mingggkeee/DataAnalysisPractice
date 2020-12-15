f6e9b27fcaed3614608857c94b49c6d536f9c74d
minkeejoo <mksu234@naver.com>
2020-12-15 14:40:23 +0900

    'master'

---
+++ exampleweb2/polls/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render, get_object_or_404
+from django.http.response import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from polls.repository import PollsRepository
+from polls.models import Question, Choice
+# Create your views here.
+
+def index(request):
+
+    # repository = PollsRepository()
+    # question_list = repository.select_questions()
+    question_list = Question.objects.all()
+
+    # print(question_list)
+    context = { 'question_list' : question_list}
+
+    return render(request, "polls/index.html", context)  # context는 index.html을 처리할 때 사용할 수 있도록 전달하는 데이터
+
+def detail(request, question_id):
+
+    # 1. Question 조회 + Choice 조회
+
+    # repository = PollsRepository()
+    # question = repository.select_question_by_id(question_id)
+    # choice_list = repository.select_choice_by_question_id(question_id)
+    # question.choices = choice_list
+
+    # question = Question.objects.get(id=question_id) # 데이터만 조회하는 코드
+
+    question = get_object_or_404(Question, pk=question_id) # 데이터 조회 + 없으면 404 오류
+
+    # 2. 템플릿으로 이동 (데이터도 함께 전달)
+    return render(request, "polls/detail.html", { 'question' : question })
+
+def vote(request, question_id):
+    # 테스트용 코드
+    # return HttpResponse("<h1>Vote for Question {0} Page</h1>".format(question_id))
+
+    # 사용자가 form에 입력해서 POST로 전송한 choice 값 읽기
+    choice_id = int(request.POST['choice']) # request.POST['name 속성의 값'] : POST로 전송된 요청 데이터를 읽는 방법 -> 문자열
+    
+    # 사용자가 선택한 choice의 votes값을 1 증가 (데이터베이스 변경 사항)
+    # repository = PollsRepository()
+    # repository.update_choice_votes(choice_id)
+
+    # question = Question.objects.get(id=question_id)
+    # choice = Question.choice_set.get(id=choice_id)
+    choice = Choice.objects.get(id=choice_id)
+    choice.votes += 1 # ORM 객체의 필드 값 변경 (데이터 변경)
+    choice.save() # 데이터베이스에 변경 내용 저장
+
+    # 투표 결과 화면으로 이동 (redirect 이동 : 브라우저로 응답을 보내는데 이 응답이 즉시 다시 서버로 새로운 요청을 보내도록 처리)
+    return HttpResponseRedirect(reverse("polls:results", args=(question_id, ))) # reverse : urls.py에 등록된 요청경로를 가져와서 사용
+
+def results(request, question_id):
+
+    # 1. Question 조회 + Choice 조회
+    question = Question.objects.get(id=question_id)
+
+    # 2. 템플릿으로 이동 (데이터도 함께 전달)
+    return render(request, "polls/results.html", { 'question' : question })
+
+   
+
+    
+    
