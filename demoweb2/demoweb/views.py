8959e9818373729cedf51d72c16df77325444922
mingggkeee <mksu234@naver.com>
2020-12-18 15:26:00 +0900

    first

--- demoweb2/demoweb/views.py
+++ demoweb2/demoweb/views.py
@@ -1,8 +1,12 @@
 from django.views.generic.base import TemplateView
 from django.views.generic import CreateView
+
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import AccessMixin
 
 from django.urls import reverse_lazy
+
+
 
 # class형 뷰를 만들때는 상속을 받아야합니다
 
@@ -19,3 +23,13 @@ class UserCreateView(CreateView):
 
 class UserCreateDoneView(TemplateView):
     template_name = 'registration/register_done.html'
+
+class OwnerOnlyMixin(AccessMixin):
+    raise_exception = True
+    permission_denied_message = "Owner only can update/delete the object"
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if self.request.user != self.object.owner:
+            self.handle_no_permission()
+        return super().get(request, *args, **kwargs)
